b9f1d8b54bb11cb73c206db055600a3c8a584a91
omar sawy <omarsawy51@gmail.com>
2025-03-15 21:59:41 +0200

    remove data anotations

--- Application/Dtos/UpdateArticleDto.cs
+++ Application/Dtos/UpdateArticleDto.cs
@@ -5,18 +5,13 @@ namespace Application.Dtos
     public class UpdateArticleDto
     {
         public int ArticleId { get; set; }
-        
-        [Required]
-        [StringLength(100)]
+
         public string Title { get; set; } = string.Empty;
-        
-        [Required]
-        [StringLength(200)]
+
         public string SubHeading { get; set; } = string.Empty;
-        
-        [Required]
+
         public string Content { get; set; } = string.Empty;
-        
+
         public int UserId { get; set; }
     }
 }
